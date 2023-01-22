34f249477e99241cb5a89515774af4c0906ea43e
kazuu <3580752+ka-zuu@users.noreply.github.com>
2023-01-22 18:13:03 +0900

    Initial Commit

---
+++ GoogleCalendar_to_Discord_Summary.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -xvu
+
+# カレンダーからイベントを取得して、今日のサマリを送信する
+
+# 初期設定
+tmp=$(mktemp)
+
+work_dir="$(dirname $0)" # スクリプトのあるディレクトリ
+filename="$(basename $0)" # スクリプトのファイル名
+mkdir -p ${work_dir}/log # ログディレクトリ
+exec 2> ${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S) # 標準エラー出力をログファイルに出力
+cd ${work_dir}
+
+# 外部の変数ファイルを取得
+source ${work_dir}/keys.sh
+
+# カレンダーイベントの取得
+while read calendar_id; do
+  python3 ${work_dir}/get_event_gcal.py ${calendar_id} > $tmp-${calendar_id}_events
+
+  # カレンダーデータのうち、本日の予定を取得する
+  today=$(date +%Y-%m-%d)
+  cat $tmp-${calendar_id}_events |
+  # 今日の日付で始まる行を取得
+  grep -e "^${today}" |
+  # 今日の日付を削除
+  sed -e "s/^${today} //" |
+  # 予定の開始時間を整形
+  sed 's/^.*T\([0-9][0-9]\):\([0-9][0-9]\).* \(.*$\)/\1:\2 \3/' > $tmp-${calendar_id}_events_today
+
+  cat ${work_dir}/target_events.txt |
+  while read event; do
+    # 通知するメッセージを作成
+    message="【${calendar_id}】${event}"
+    # discordに通知
+    curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"${message}\"}" ${DISCORD_WEBHOOK_URL}
+  done < $tmp-${calendar_id}_events_today
+done
+
+# 一時ファイルの削除
+rm -f $tmp-*
+
+# 終了
+exit 0
---
+++ GoogleCalendar_to_Discord_Updates.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -xvu
+
+# カレンダーからイベントを取得して、差分をDiscordに通知する
+
+# 初期設定
+tmp=$(mktemp)
+
+work_dir="$(dirname $0)" # スクリプトのあるディレクトリ
+filename="$(basename $0)" # スクリプトのファイル名
+mkdir -p ${work_dir}/log # ログディレクトリ
+exec 2> ${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S) # 標準エラー出力をログファイルに出力
+cd ${work_dir}
+
+# 外部の変数ファイルを取得
+source ${work_dir}/keys.sh
+
+# カレンダーイベントの取得
+cat ${work_dir}/target_calendars.txt |
+while read calendar_id; do
+  python3 ${work_dir}/get_event_gcal.py ${calendar_id} > $tmp-${calendar_id}_events
+
+  # 過去のデータを取得して、差分を抽出
+  if [ -s ${work_dir}/events_old/${calendar_id}_events ]; then
+    diff ${work_dir}/events_old/${calendar_id}_events $tmp-${calendar_id}_events > $tmp-${calendar_id}_diff
+  fi
+
+  # 差分があればDiscordに通知
+  if [ -s $tmp-${calendar_id}_diff ]; then
+    while read line; do
+      if [ "${line:0:1}" = ">" ]; then
+        # Discordに通知
+        curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"${line:2}\"}" ${DISCORD_WEBHOOK_URL}
+      fi
+    done < $tmp-${calendar_id}_diff
+  fi
+
+  # 今回の結果を過去データとして保存
+  mkdir -p ${work_dir}/events_old
+  mv $tmp-${calendar_id}_events ${work_dir}/events_old/${calendar_id}_events
+
+done
+
+# 一時ファイルの削除
+rm -f $tmp-*
+
+# 終了
+exit 0
