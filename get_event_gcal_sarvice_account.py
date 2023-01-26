9f9a070aa1189655efec4bb99882925a39a70998
ka-zuu <3580752+ka-zuu@users.noreply.github.com>
2023-01-27 07:29:01 +0900

    サービスアカウントから取得するPythonを作成

---
+++ get_event_gcal_sarvice_account.py
@@ -0,0 +1,31 @@
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+
+import datetime
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide a calendar ID as a command line argument.")
+        return
+    calendar_id = sys.argv[1]
+
+    creds = Credentials.from_service_account_file('credentials.json', scopes=['https://www.googleapis.com/auth/calendar.events'])
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        events_result = service.events().list(calendarId=calendar_id, timeMin=now, singleEvents=True, orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+if __name__ == '__main__':
+    main()
