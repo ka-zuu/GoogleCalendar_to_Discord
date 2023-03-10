1e3bc05d6ebe356d243a999f80a7c423c390607a
kazuu <3580752+ka-zuu@users.noreply.github.com>
2023-03-10 10:52:31 +0900

    Shellcheckの指摘に対応してみた

--- GoogleCalendar_to_Discord_Summary.sh
+++ GoogleCalendar_to_Discord_Summary.sh
@@ -5,49 +5,49 @@
 # 初期設定
 tmp=$(mktemp)
 
-work_dir="$(dirname $0)" # スクリプトのあるディレクトリ
-filename="$(basename $0)" # スクリプトのファイル名
-mkdir -p ${work_dir}/log # ログディレクトリ
-exec 2> ${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S) # 標準エラー出力をログファイルに出力
-cd ${work_dir}
+work_dir=$(dirname "$0") # スクリプトのあるディレクトリ
+filename=$(basename "$0") # スクリプトのファイル名
+mkdir -p "${work_dir}/log" # ログディレクトリ
+exec 2> "${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S)" # 標準エラー出力をログファイルに出力
+cd "${work_dir}" || exit 1
 
 # 外部の変数ファイルを取得
-source ${work_dir}/keys.sh
+# shellcheck source=/dev/null
+source "${work_dir}/keys.sh"
 
 # カレンダーイベントの取得
-cat ${work_dir}/target_calendars.txt |
-while read calendar_id; do
-  python3 ${work_dir}/get_event_gcal_service_account.py ${calendar_id} > $tmp-${calendar_id}_events
+while read -r calendar_id; do
+  python3 "${work_dir}/get_event_gcal_service_account.py" "${calendar_id}" > "$tmp-${calendar_id}_events"
 
   # カレンダーデータのうち、本日の予定を取得する
   today=$(date +%Y-%m-%d)
-  cat $tmp-${calendar_id}_events |
+  cat "$tmp-${calendar_id}_events" |
   # 今日の日付で始まる行を取得
   grep -e "^${today}" |
   # 予定の開始時間を整形
   sed 's/^.*T\([0-9][0-9]\):\([0-9][0-9]\).* \(.*$\)/\1:\2 \3/' |
-  awk '{if(NF==1){print "終日",$0}else{print $0}}' > $tmp-${calendar_id}_today
+  awk '{if(NF==1){print "終日",$0}else{print $0}}' > "$tmp-${calendar_id}_today"
 
-  if [ -s $tmp-${calendar_id}_today ]; then
+  if [ -s "$tmp-${calendar_id}_today" ]; then
     # 今日の予定がある場合、各行にカレンダー名を付与して、1行にまとめる
-    cat <(echo "【${calendar_id}】" ) $tmp-${calendar_id}_today >> $tmp-today_summary
+    cat <(echo "【${calendar_id}】" ) "$tmp-${calendar_id}_today" >> "$tmp-today_summary"
 
   else
     # 今日の予定がない場合、Discordに通知
-    echo -e "【${calendar_id}】\n今日の予定はありません" >> $tmp-today_summary
+    echo -e "【${calendar_id}】\n今日の予定はありません" >> "$tmp-today_summary"
   fi
-done
+done < "${work_dir}/target_calendars.txt"
 
 # Discordに通知
-cat $tmp-today_summary |
+cat "$tmp-today_summary" |
 # 改行を削除して、一行にまとめる
 sed "s/$/\\\n/" |
-tr -d "\n" > $tmp-today_summary2
+tr -d "\n" > "$tmp-today_summary2"
 
-curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"$(cat $tmp-today_summary2)\"}" ${DISCORD_WEBHOOK_URL}
+curl -X POST -H "Content-Type: application/json" -d '{"content": "'"$(cat $tmp-today_summary2)"'"}' "${DISCORD_WEBHOOK_URL}"
 
 # 一時ファイルの削除
-rm -f $tmp-*
+rm -f "$tmp"-*
 
 # 終了
 exit 0
--- GoogleCalendar_to_Discord_Updates.sh
+++ GoogleCalendar_to_Discord_Updates.sh
@@ -7,53 +7,53 @@ tmp=$(mktemp)
 
 work_dir="$(dirname $0)" # スクリプトのあるディレクトリ
 filename="$(basename $0)" # スクリプトのファイル名
-mkdir -p ${work_dir}/log # ログディレクトリ
-exec 2> ${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S) # 標準エラー出力をログファイルに出力
-cd ${work_dir}
+mkdir -p "${work_dir}/log" # ログディレクトリ
+exec 2> "${work_dir}/log/${filename}.$(date +%Y%m%d_%H%M%S)" # 標準エラー出力をログファイルに出力
+cd "${work_dir}" || exit 1
 
 # 外部の変数ファイルを取得
-source ${work_dir}/keys.sh
+# shellcheck source=/dev/null
+source "${work_dir}/keys.sh"
 
 # カレンダーイベントの取得
-cat ${work_dir}/target_calendars.txt |
-while read calendar_id; do
-  python3 ${work_dir}/get_event_gcal_service_account.py ${calendar_id} > $tmp-${calendar_id}_events
+while read -r calendar_id; do
+  python3 "${work_dir}/get_event_gcal_service_account.py" "${calendar_id}" > "$tmp-${calendar_id}_events"
 
   # 過去のデータを取得して、差分を抽出
-  if [ -s ${work_dir}/events_old/${calendar_id}_events ]; then
+  if [ -s "${work_dir}/events_old/${calendar_id}_events" ]; then
     # 過去のデータから昨日の日付を含む行は除く
-    cat ${work_dir}/events_old/${calendar_id}_events |
+    cat "${work_dir}/events_old/${calendar_id}_events" |
     grep -v "$(date -d "1 day ago" +%Y-%m-%d)" |
     # 差分を取る
-    diff - $tmp-${calendar_id}_events > $tmp-${calendar_id}_diff
+    diff - "$tmp-${calendar_id}_events" > "$tmp-${calendar_id}_diff"
   fi
 
   # 差分があればDiscordに通知するために追記
-  if [ -s $tmp-${calendar_id}_diff ]; then
-    cat $tmp-${calendar_id}_diff |
+  if [ -s "$tmp-${calendar_id}_diff" ]; then
+    cat "$tmp-${calendar_id}_diff" |
     # 予定の日時を整形
     sed 's/^\(.*\)T\([0-9][0-9]\):\([0-9][0-9]\).* \(.*$\)/\1 \2:\3 \4/' |
     # >か<で始まる行だけを取得
     grep -e "^>" -e "^<" |
     # 差分を日本語にする
     sed -e 's/^>/追加/' -e 's/^</削除/' |
-    cat <(echo "【${calendar_id}】") - >> $tmp-diff_for_send
+    cat <(echo "【${calendar_id}】") - >> "$tmp-diff_for_send"
   fi
 
   # 今回の結果を過去データとして保存
-  mkdir -p ${work_dir}/events_old
-  mv $tmp-${calendar_id}_events ${work_dir}/events_old/${calendar_id}_events
-done
+  mkdir -p "${work_dir}/events_old"
+  mv "$tmp-${calendar_id}_events" "${work_dir}/events_old/${calendar_id}_events"
+done < "${work_dir}/target_calendars.txt"
 
 # 送信する差分があれば、Discordに通知
-if [ -s $tmp-diff_for_send ]; then
-  cat $tmp-diff_for_send |
+if [ -s "$tmp-diff_for_send" ]; then
+  cat "$tmp-diff_for_send" |
   # 改行を削除して、一行にまとめる
   sed "s/$/\\\n/" |
-  tr -d "\n" > $tmp-diff_for_send2
+  tr -d "\n" > "$tmp-diff_for_send2"
 
   # Discordに通知
-  curl -X POST -H "Content-Type: application/json" -d '{"content": "'"$(cat $tmp-diff_for_send2)"'"}' ${DISCORD_WEBHOOK_URL}
+  curl -X POST -H "Content-Type: application/json" -d '{"content": "'"$(cat $tmp-diff_for_send2)"'"}' "${DISCORD_WEBHOOK_URL}"
 fi
 
 # 一時ファイルの削除
